9779b0bc90dbb9c5613a77ca1c31061b0925d00f
Stiven C <08.cacais.willian.803@gmail.com>
2026-04-19 16:49:38 -0500

    proteger credenciales, añadir preguntas desde administracion

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
@@ -12,19 +13,19 @@ app.use(express.json());
 app.use(express.static(__dirname));
 
 cloudinary.config({
-    cloud_name: 'dgnivrsl6',
-    api_key: '582391443693159',
-    api_secret: 'i5MYuMINxbxXuB-jvpwNsNSWDs0'
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
 // ✅ Multer en memoria  👈 AQUÍ
 const upload = multer({ storage: multer.memoryStorage() })
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'simulacro_icfes'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
